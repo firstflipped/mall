0dc5aa5b4729c87f8730b00b85d108054b7e1397
wangjie <laughingather@gmail.com>
2020-12-06 19:18:58 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/laughingather/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laughingather.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
