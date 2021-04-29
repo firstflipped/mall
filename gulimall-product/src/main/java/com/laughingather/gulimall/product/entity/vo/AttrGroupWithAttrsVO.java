63b01b5fb7616f64ce38edecb5479aa983f75f1f
laughingather <laughingather@gmail.com>
2021-04-30 01:10:28 +0800

    库存功能优化   采购功能实现

---
+++ gulimall-product/src/main/java/com/laughingather/gulimall/product/entity/vo/AttrGroupWithAttrsVO.java
@@ -0,0 +1,38 @@
+package com.laughingather.gulimall.product.entity.vo;
+
+import com.laughingather.gulimall.product.entity.AttrEntity;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AttrGroupWithAttrsVO {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 分组id
+     */
+    private Long attrGroupId;
+    /**
+     * 组名
+     */
+    private String attrGroupName;
+    /**
+     * 排序
+     */
+    private Integer sort;
+    /**
+     * 描述
+     */
+    private String descript;
+    /**
+     * 组图标
+     */
+    private String icon;
+    /**
+     * 所属分类id
+     */
+    private Long catelogId;
+
+    private List<AttrEntity> attrs;
+}
