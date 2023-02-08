4ee0fd41fc03d3cfbdb740d4e6bf497b05fb6463
flipped <flipped@163.com>
2023-02-08 09:12:00 +0800

    feat: 增加商城爬虫服务模块

---
+++ mall-reptile/src/main/java/com/flipped/mall/reptile/MallReptileApplication.java
@@ -0,0 +1,20 @@
+package com.flipped.mall.reptile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 爬虫服务启动类
+ *
+ * @author <a href="#">flipped</a>
+ * @version v1.0
+ * @since ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MallReptileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallReptileApplication.class, args);
+    }
+}
