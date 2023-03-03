ad4a2f71ce1dad97848fd9f2113223c38f4f9d48
flipped <firstflipped@163.com>
2023-03-03 08:34:42 +0800

    feat: 实现Spring Security自定义捕获异常功能

---
+++ mall-admin/src/main/java/com/flipped/mall/admin/handle/AccessDeniedHandlerImpl.java
@@ -0,0 +1,37 @@
+package com.flipped.mall.admin.handle;
+
+import com.flipped.mall.common.entity.api.MyResult;
+import com.flipped.mall.common.util.JsonUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/**
+ * 自定义权限异常处理
+ * 该配置不生效，异常由全局统一异常拦截处理器处理：com.flipped.mall.admin.handle.MallAdminExceptionControllerAdvice
+ *
+ * @author <a href="#">flipped</a>
+ * @version v1.0
+ * @since 2023-03-01 20:34:09
+ */
+public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        MyResult<Void> result = MyResult.<Void>builder()
+                .code(HttpStatus.FORBIDDEN.value())
+                .message("用户权限不足")
+                .success(false)
+                .timestamp(LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli())
+                .build();
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(JsonUtil.bean2Json(result));
+    }
+
+}
---
+++ mall-admin/src/main/java/com/flipped/mall/admin/handle/AuthenticationEntryPointImpl.java
@@ -0,0 +1,38 @@
+package com.flipped.mall.admin.handle;
+
+import com.flipped.mall.common.entity.api.MyResult;
+import com.flipped.mall.common.util.JsonUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/**
+ * 自定义认证异常处理
+ *
+ * @author <a href="#">flipped</a>
+ * @version v1.0
+ * @since 2023-03-01 20:12:36
+ */
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        MyResult<Void> result = MyResult.<Void>builder()
+                .code(HttpStatus.UNAUTHORIZED.value())
+                .message("用户认证失败")
+                .success(false)
+                .timestamp(LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli())
+                .build();
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(JsonUtil.bean2Json(result));
+    }
+
+}
+
